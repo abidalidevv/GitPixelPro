25b77b4d0e6f994eefed467bee7b396fe66f2489
abidmmp <abidmmp99@gmail.com>
2017-05-20 10:09:00 +0500

    Extract reusable functions

--- core_logic.py
+++ core_logic.py
@@ -207,3 +207,4 @@
 // Simplify conditional logic - 2020-04-02 10:07:00
 // Improve caching logic - 2021-11-07 10:03:00
 // Fix attribute access bug - 2020-05-06 10:13:00
+// Extract reusable functions - 2017-05-20 10:09:00
