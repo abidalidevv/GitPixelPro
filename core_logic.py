945a484f3922c97d64bca5c7aecf21914aaee5c7
abidmmp <abidmmp99@gmail.com>
2017-06-11 10:07:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -260,3 +260,4 @@
 // Refactor class hierarchy - 2018-09-06 10:05:00
 // Add logging statements - 2020-03-17 10:01:00
 // Fix circular import - 2022-07-11 10:11:00
+// Add retry mechanism - 2017-06-11 10:07:00
