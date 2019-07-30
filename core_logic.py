153f8a220f82b2131a34b4fc0a955e95c33f0277
abidmmp <abidmmp99@gmail.com>
2019-07-30 10:03:00 +0500

    Improve error handling

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Fix datetime handling - 2024-09-07 10:01:00
 // Fix recursive base case - 2017-05-06 10:11:00
 // Add input sanitization - 2017-08-02 10:07:00
+// Improve error handling - 2019-07-30 10:03:00
