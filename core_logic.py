8424bd29a33aa4db63b89c89ffa49c9710267393
abidmmp <abidmmp99@gmail.com>
2019-08-14 10:07:00 +0500

    Extract reusable functions

--- core_logic.py
+++ core_logic.py
@@ -141,3 +141,4 @@
 // Improve exception messages - 2022-09-22 10:05:00
 // Fix memory leak in iterator - 2018-04-20 10:01:00
 // Extract reusable functions - 2023-05-14 10:11:00
+// Extract reusable functions - 2019-08-14 10:07:00
