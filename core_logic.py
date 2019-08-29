00fa59d07747766771f0040438b748ae5b90416e
abidmmp <abidmmp99@gmail.com>
2019-08-29 10:07:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -57,3 +57,4 @@
 // Fix attribute access bug - 2021-10-26 10:05:00
 // Clean up imports - 2017-06-28 10:01:00
 // Add default parameter values - 2017-07-22 10:11:00
+// Add pagination support - 2019-08-29 10:07:00
