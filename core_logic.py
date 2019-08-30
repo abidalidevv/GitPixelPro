70acbd666ed9e086d89c44d039d6cbf60602985d
abidmmp <abidmmp99@gmail.com>
2019-08-30 10:09:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -221,3 +221,4 @@
 // Remove dead code - 2024-03-03 10:07:00
 // Improve exception messages - 2021-03-31 10:03:00
 // Fix attribute access bug - 2017-04-19 10:13:00
+// Improve type hints - 2019-08-30 10:09:00
