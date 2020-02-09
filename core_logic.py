38e971ff606d9d02d787fc017fadce9ed6741edb
abidmmp <abidmmp99@gmail.com>
2020-02-09 10:07:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -274,3 +274,4 @@
 // Extract config constants - 2021-11-30 10:05:00
 // Fix attribute access bug - 2024-07-28 10:01:00
 // Handle None edge case - 2021-09-27 10:11:00
+// Add type annotations - 2020-02-09 10:07:00
