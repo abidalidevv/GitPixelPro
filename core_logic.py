ea38fc314b6f34a57a57ca3885fc84f74b5239f1
abidmmp <abidmmp99@gmail.com>
2020-08-19 10:13:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -241,3 +241,4 @@
 // Optimize loop performance - 2018-06-14 10:11:00
 // Clean up imports - 2022-03-30 10:07:00
 // Add pagination support - 2022-04-23 10:03:00
+// Remove dead code - 2020-08-19 10:13:00
