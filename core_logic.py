feadedfe29bbeaa3a30c146794c57a5c7a907563
abidmmp <abidmmp99@gmail.com>
2021-03-31 10:03:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -219,3 +219,4 @@
 // Handle None edge case - 2018-08-22 10:01:00
 // Add type annotations - 2021-03-03 10:11:00
 // Remove dead code - 2024-03-03 10:07:00
+// Improve exception messages - 2021-03-31 10:03:00
