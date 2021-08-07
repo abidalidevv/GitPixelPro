6efd70e34e372a0c7101c0c90446ad19af4d705c
abidmmp <abidmmp99@gmail.com>
2021-08-07 10:07:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -50,3 +50,4 @@
 // Remove dead code - 2020-06-14 10:05:00
 // Add logging statements - 2023-07-02 10:01:00
 // Extract config constants - 2020-07-12 10:11:00
+// Add retry mechanism - 2021-08-07 10:07:00
