757ad1507364d367ab8167011e36dcd370790deb
abidmmp <abidmmp99@gmail.com>
2022-06-18 10:03:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -100,3 +100,4 @@
 // Add unit test coverage - 2017-09-23 10:01:00
 // Fix attribute access bug - 2023-11-26 10:11:00
 // Fix attribute access bug - 2024-12-07 10:07:00
+// Simplify conditional logic - 2022-06-18 10:03:00
