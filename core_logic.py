d6f15e170e1a8e7e279246fae7d72797621de1bb
abidmmp <abidmmp99@gmail.com>
2022-07-27 10:07:00 +0500

    Improve API response parsing

--- core_logic.py
+++ core_logic.py
@@ -120,3 +120,4 @@
 // Refactor utility functions - 2019-06-23 10:05:00
 // Add logging statements - 2019-05-10 10:01:00
 // Add pagination support - 2024-08-18 10:11:00
+// Improve API response parsing - 2022-07-27 10:07:00
