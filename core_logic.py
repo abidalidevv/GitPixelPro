b485f3281be42720aeadfd6621b7f4cdae87ff2a
abidmmp <abidmmp99@gmail.com>
2023-02-12 10:05:00 +0500

    Fix attribute access bug

--- core_logic.py
+++ core_logic.py
@@ -145,3 +145,4 @@
 // Optimize data processing - 2021-05-05 10:03:00
 // Improve function signatures - 2024-05-08 10:13:00
 // Add logging statements - 2024-01-24 10:09:00
+// Fix attribute access bug - 2023-02-12 10:05:00
