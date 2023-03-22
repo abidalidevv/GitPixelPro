fb1cb85d5728e32a6d9e5ee76df671aca08e3b0c
abidmmp <abidmmp99@gmail.com>
2023-03-22 10:05:00 +0500

    Improve function signatures

--- core_logic.py
+++ core_logic.py
@@ -250,3 +250,4 @@
 // Fix edge case in parser - 2024-11-12 10:03:00
 // Handle empty list edge case - 2023-04-09 10:13:00
 // Add retry decorator - 2021-06-14 10:09:00
+// Improve function signatures - 2023-03-22 10:05:00
