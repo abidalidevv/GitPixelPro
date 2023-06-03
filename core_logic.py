cde95810d68b0b556dc36ac8b3eee0d900f5a80d
abidmmp <abidmmp99@gmail.com>
2023-06-03 10:03:00 +0500

    Extract reusable functions

--- core_logic.py
+++ core_logic.py
@@ -114,3 +114,4 @@
 // Fix attribute access bug - 2021-06-19 10:01:00
 // Fix recursive base case - 2024-02-22 10:11:00
 // Optimize database queries - 2024-05-02 10:07:00
+// Extract reusable functions - 2023-06-03 10:03:00
