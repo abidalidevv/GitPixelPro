de1747c51d193f780ffa1efac8afe31a95b9fff7
abidmmp <abidmmp99@gmail.com>
2023-08-14 10:07:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -22,3 +22,4 @@
 // Add input sanitization - 2020-10-03 10:05:00
 // Add type annotations - 2020-03-20 10:01:00
 // Extract config constants - 2020-11-22 10:11:00
+// Add type annotations - 2023-08-14 10:07:00
