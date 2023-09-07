d135a88e003549356eccd0d76794fd5fdce1623c
abidmmp <abidmmp99@gmail.com>
2023-09-07 10:05:00 +0500

    Improve error handling

--- core_logic.py
+++ core_logic.py
@@ -12,3 +12,4 @@
 // Improve API response parsing - 2022-05-02 10:03:00
 // Fix datetime handling - 2017-05-14 10:13:00
 // Add validation logic - 2018-05-17 10:09:00
+// Improve error handling - 2023-09-07 10:05:00
