ac8ec709e695047c206f09b1f8ca79629d006b0d
abidmmp <abidmmp99@gmail.com>
2023-11-26 10:11:00 +0500

    Fix attribute access bug

--- core_logic.py
+++ core_logic.py
@@ -98,3 +98,4 @@
 // Clean up imports - 2022-06-22 10:09:00
 // Improve error handling - 2021-09-01 10:05:00
 // Add unit test coverage - 2017-09-23 10:01:00
+// Fix attribute access bug - 2023-11-26 10:11:00
