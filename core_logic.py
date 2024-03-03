4402aa91a5ddb825d8da317189b574f5cb138127
abidmmp <abidmmp99@gmail.com>
2024-03-03 10:07:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -218,3 +218,4 @@
 // Add type annotations - 2020-08-30 10:05:00
 // Handle None edge case - 2018-08-22 10:01:00
 // Add type annotations - 2021-03-03 10:11:00
+// Remove dead code - 2024-03-03 10:07:00
