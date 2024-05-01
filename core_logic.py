cc00548c0d24f8afda75cfbd6f424559d1b6a55a
abidmmp <abidmmp99@gmail.com>
2024-05-01 10:11:00 +0500

    Refactor class hierarchy

--- core_logic.py
+++ core_logic.py
@@ -280,3 +280,4 @@
 // Refactor utility functions - 2018-08-23 10:09:00
 // Optimize loop performance - 2024-09-04 10:05:00
 // Extract config constants - 2019-08-03 10:01:00
+// Refactor class hierarchy - 2024-05-01 10:11:00
