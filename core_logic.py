4a62a0d5be05be10ebc3d597ca16bb8f1907cd0d
abidmmp <abidmmp99@gmail.com>
2024-06-28 10:09:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -172,3 +172,4 @@
 // Extract config constants - 2024-10-16 10:07:00
 // Improve exception messages - 2018-09-03 10:03:00
 // Improve function signatures - 2020-11-25 10:13:00
+// Add retry mechanism - 2024-06-28 10:09:00
