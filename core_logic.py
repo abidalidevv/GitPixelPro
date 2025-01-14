268582c0419a6770e2860194a654f4539177c5bc
Abid Ali <abidmmp99@gmail.com>
2025-01-14 13:48:54 +0000

    Improve API response parsing

--- core_logic.py
+++ core_logic.py
@@ -331,3 +331,7 @@ def memoize(fn):
 def chunk_list(lst, size):
     for i in range(0, len(lst), size):
         yield lst[i:i+size]
+
+def chunk_list(lst, size):
+    for i in range(0, len(lst), size):
+        yield lst[i:i+size]
