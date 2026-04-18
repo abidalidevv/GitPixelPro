9e7fe6d41d8a1c7711ab250746e9c4255d825711
Abid Ali <abidmmp99@gmail.com>
2026-04-18 12:37:21 +0000

    Add unit test coverage

--- core_logic.py
+++ core_logic.py
@@ -535,3 +535,10 @@ def parse_bool(v):
 
 def get_env(key, default=''):
     import os; return os.environ.get(key, default)
+
+def memoize(fn):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache: cache[args] = fn(*args)
+        return cache[args]
+    return wrapper
